56a889f70cef8fcbc6256cf4f036bda8b100e4ba
Verschynin Ivan <v21112005@gmail.com>
2025-10-01 19:25:39 +0300

    DeckCardPage Progress-block init

--- src/Pages/DeckPage/DeckPage.tsx
+++ src/Pages/DeckPage/DeckPage.tsx
@@ -1,7 +1,10 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './DeckPage.css'
 import { useParams } from 'react-router-dom'
 import type { Deck } from '../../types/Deck'
+import axios from 'axios'
+import { Target, Check , X } from 'lucide-react'
+import './DeckPage.css'
 
 interface DeckPageProps{
     decks:Deck[]
@@ -11,16 +14,37 @@ const DeckPage:React.FunctionComponent<DeckPageProps> = ({decks}) => {
   
     const {deckId} = useParams()
 
-    const currentDeck = decks.find(deck => deck.id === Number(deckId))
-    console.log(currentDeck)
+    const [correctCount,setCorrectCount] = useState(0)
+    const [wrongCount,wrongtCount] = useState(0)
+    const [currentDeck, setCurrentDeck] = useState<Deck | null>(null)
+    const [answersCount, setAnswersCount] = useState(0)
+
+
+
+    const fetchCurrentDeck = async (id:typeof deckId) =>{
+        const res =  await axios.get<Deck>('https://68dbf0f1445fdb39dc2727be.mockapi.io/decks/'+deckId)
+        return res.data 
+    }
+
+    useEffect(()=>{
+        fetchCurrentDeck(deckId).then(deck => setCurrentDeck(deck))
+    },[deckId])
+
+    const cardsCount = currentDeck?.cards.length
+
 
   return (
     <div className='deck-cards'>
         <div className='deck-cards__wrapper'>
             <div className='deck-cards__progress'>
-                {
-                    currentDeck?.title
-                }
+                <p> <Target/> Study progress  {answersCount}/{cardsCount}</p>
+                <div className='deck-cards__progress__answers'>
+                        <p className='deck-cards__propgress__answers-elem'><Check className='deck-cards__propgress__answers-elem-correct'/>{correctCount} <br /> Правильно</p>
+                        <p className='deck-cards__propgress__answers-elem'><X className='deck-cards__propgress__answers-elem-wrong'/> {wrongCount} <br />  Неправильно</p>
+                </div>
+            </div>
+            <div className='deck-cards__cards__wrapper'>
+
             </div>
         </div>
     </div>
