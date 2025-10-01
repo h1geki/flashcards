0d53e20997e7d510f9481f2d31a6f09560f6a91f
Verschynin Ivan <v21112005@gmail.com>
2025-10-01 22:36:41 +0300

    DeckCardPage end

--- src/Pages/DeckPage/DeckPage.tsx
+++ src/Pages/DeckPage/DeckPage.tsx
@@ -5,6 +5,9 @@ import type { Deck } from '../../types/Deck'
 import axios from 'axios'
 import { Target, Check , X } from 'lucide-react'
 import './DeckPage.css'
+import CardComponent from '../../components/Card/CardComponent'
+import type { Card } from '../../types/Card'
+import MyButton from '../../components/ui/MyButton/MyButton'
 
 interface DeckPageProps{
     decks:Deck[]
@@ -15,9 +18,13 @@ const DeckPage:React.FunctionComponent<DeckPageProps> = ({decks}) => {
     const {deckId} = useParams()
 
     const [correctCount,setCorrectCount] = useState(0)
-    const [wrongCount,wrongtCount] = useState(0)
+    const [wrongCount,setWrongtCount] = useState(0)
     const [currentDeck, setCurrentDeck] = useState<Deck | null>(null)
     const [answersCount, setAnswersCount] = useState(0)
+    const [currentCard, setCurrentCard] = useState<Card | undefined>(undefined)
+    const [currentSideIndex , setCurrentSideIndex] = useState<1 | 0>(0)
+    const [cardIndex, setCardIndex] = useState(0)
+    const [isResult, setIsPesult] = useState(false)
 
 
 
@@ -27,26 +34,85 @@ const DeckPage:React.FunctionComponent<DeckPageProps> = ({decks}) => {
     }
 
     useEffect(()=>{
-        fetchCurrentDeck(deckId).then(deck => setCurrentDeck(deck))
+        fetchCurrentDeck(deckId).then(deck => {
+            setCurrentDeck(deck)
+            if(deck){
+                setCurrentCard(deck.cards[cardIndex])
+            }
+        })
     },[deckId])
 
     const cardsCount = currentDeck?.cards.length
 
+    const handleCardClick = () =>{
+        setCurrentSideIndex(currentSideIndex === 1 ? 0 : 1 )
+    }
+    const handleCorrectClick = () => {
+        setCorrectCount(correctCount + 1)
+        setAnswersCount(answersCount+1)
+        cardIndexSwitcher()
+        
+    }
+    const handleWrongClick = () => {
+        setWrongtCount(wrongCount + 1)
+        setAnswersCount(answersCount+1)
+        cardIndexSwitcher()
+        
+        
+    }
+
+    const cardIndexSwitcher = () => {
+
+        if(currentDeck?.cards && cardIndex >= currentDeck?.cards.length - 1){
+            setIsPesult(true)
+            return
+        }
+
+        const newIndex = cardIndex + 1
+        setCardIndex(newIndex)
+        setCurrentCard(currentDeck?.cards[newIndex])
+    }
+
+    const restart = () => {
+        setAnswersCount(0)
+        setCorrectCount(0)
+        setWrongtCount(0)
+        setCardIndex(0)
+        setCurrentSideIndex(0)
+        setIsPesult(false)
+        setCurrentCard(currentDeck?.cards[0])
+    }
 
   return (
     <div className='deck-cards'>
-        <div className='deck-cards__wrapper'>
-            <div className='deck-cards__progress'>
-                <p> <Target/> Study progress  {answersCount}/{cardsCount}</p>
-                <div className='deck-cards__progress__answers'>
-                        <p className='deck-cards__propgress__answers-elem'><Check className='deck-cards__propgress__answers-elem-correct'/>{correctCount} <br /> Правильно</p>
-                        <p className='deck-cards__propgress__answers-elem'><X className='deck-cards__propgress__answers-elem-wrong'/> {wrongCount} <br />  Неправильно</p>
+        
+            
+            <div className='deck-cards__wrapper'>
+                <div className='deck-cards__progress'>
+                    <p> <Target/> Study progress  {answersCount}/{cardsCount}</p>
+                    <div className='deck-cards__progress__answers'>
+                            <p className='deck-cards__propgress__answers-elem'><Check className='deck-cards__propgress__answers-elem-correct'/>{correctCount} <br /> Правильно</p>
+                            <p className='deck-cards__propgress__answers-elem'><X className='deck-cards__propgress__answers-elem-wrong'/> {wrongCount} <br />  Неправильно</p>
+                    </div>
                 </div>
+                {
+                    isResult 
+                ? 
+                <div className='deck-cards__result'>
+                    <p>Deck completed! 🎉</p>
+                    <span>You got {correctCount} correct answers of {cardsCount}</span>
+                    <button onClick={() => restart()}>Restart</button>
+                </div> 
+                : 
+                <div className='deck-cards__cards__wrapper'>
+                    <p className='deck-cards__cards__wrapper__title'>Card {answersCount+1} of {cardsCount}</p>
+                    <p className='deck-cards__cards__wrapper__descr'>Learning deck "{currentDeck?.title}"</p>
+                    <CardComponent handleCorrectClick={handleCorrectClick} handleWrongClick={handleWrongClick} handleCardClick={handleCardClick} currentCard = {currentCard} setCurrentCard = {setCurrentCard} currentSideIndex={currentSideIndex}/>
+                </div>
+                }
             </div>
-            <div className='deck-cards__cards__wrapper'>
 
-            </div>
-        </div>
+        
     </div>
   )
 }
---
+++ src/components/Card/CardComponent.tsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import type { Card} from '../../types/Card'
+import './CardComponent.css'
+import { Check, X } from 'lucide-react'
+
+interface CardProps{
+  currentCard:Card | undefined,
+  setCurrentCard:(card:Card) => void,
+  currentSideIndex: 1 | 0,
+  handleCardClick:() => void,
+  handleCorrectClick:() => void,
+  handleWrongClick:() => void
+}
+
+const CardComponent:React.FunctionComponent<CardProps> = ({currentCard, currentSideIndex,handleCardClick, handleCorrectClick,handleWrongClick}) => {
+  return (
+    <div className='card' onClick={() => handleCardClick()}>
+      {
+        !currentSideIndex ? <div className='card__first-side cardCur'>{currentCard?.firstSide}</div> 
+        : 
+        <div className='card__second-side cardCur'>{currentCard?.secondSide} 
+          <div className='card__second-side__btns'>
+            <button onClick={() => handleWrongClick() } className='card__second-side__btns-elem btn-wrong'><X size={17}/>Wrong</button>
+            <button onClick={() => handleCorrectClick()} className='card__second-side__btns-elem btn-correct'><Check/>Correct</button>
+          </div>
+        </div>
+      }
+    </div>
+  )
+}
+
+export default CardComponent
